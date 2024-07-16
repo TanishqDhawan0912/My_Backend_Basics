0dca88f1f2cc9319a9c0c50f3cc0c3f0aa0ff8c2
Tanishq Dhawan <tanishq897.be22@chitkara.edu.in>
2024-07-16 19:35:50 +0530

    first project done

--- structureexpress/index.js
+++ structureexpress/index.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("tanishqdhawan09");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>hello how are u</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
